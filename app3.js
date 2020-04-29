f426086caa1a5bb8e5e716d629dd6c9221b23bc6
storm <perez.hernandez.estela.1n@gmail.com>
2020-04-29 14:39:06 -0500

    referencia

---
+++ app3.js
@@ -0,0 +1,31 @@
+
+
+
+var a = 10;
+var b = a;
+
+console.log("a: ",a);
+console.log("b: ",b);
+
+a = 20;
+
+console.log("a: ",a);
+console.log("b: ",b);
+
+var c={
+    nombre:"juan"
+}
+var d=c;
+
+console.log("c: ",c);
+console.log("d: ",d);
+
+c.nombre="maria";
+
+console.log("c: ",c);
+console.log("d: ",d);
+
+c.nombre="pedro";
+
+console.log("c: ",c);
+console.log("d: ",d);
